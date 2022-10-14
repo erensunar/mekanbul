6a27537e7937d1489c0d5380a2b495e59fe6c0e9
Eren Sunar <kerensunar@outlook.com>
2022-10-14 14:09:31 +0300

    düzeltme 2

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,23 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 const anaSayfa = function (req, res, next) {
-  res.render('anasayfa', { title: 'Ana Sayfa' });
-};
-
-const mekanBilgisi = function (req, res, next) {
-  res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
-};
-
-const yorumEkle = function (req, res, next) {
-  res.render('yorumEkle', { title: 'Yorum ekle  ' });
-};
-
-
-module.exports = {
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle
+    res.render('anasayfa', { title: 'Ana Sayfa' })
 }
+const mekanBilgisi = function (req, res, next) {
+    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' })
+}
+const yorumEkle = function (req, res, next) {
+    res.render('yorumekle', { title: 'Yorum ekle' })
+}
+module.exports = {
 
+    anaSayfa,
+    mekanBilgisi,
+    yorumEkle
+
+
+}
